2dc11a673cf51c430fde5efdf18c1140924b7203
jack266327 <Black Jack@192.168.1.6>
2017-03-10 01:08:53 -0600

    primer avance creacion de las java class y abtract class

---
+++ Figura/src/figura.java
@@ -0,0 +1,21 @@
+
+import isc.isma4.IFigura.IFigura;
+
+
+/**
+ *
+ * @author Black Jack
+ */
+public class figura {
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+       
+        
+    }
+    public double getFigureArea(IFigura figura){
+        return figura.getArea();
+}
+}
